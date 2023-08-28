a5ba8df541a14c4703287e0e154120dcd849e100
ghost-cas <esike.akpos@gmail.com>
2023-08-28 13:31:07 +0300

    new commit 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import {ComponentPreviews, useInitial} from "./dev";
+import { DevSupport } from "@react-buddy/ide-toolbox";
+import { ComponentPreviews, useInitial } from "./dev";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-            <App/>
-        </DevSupport>
-    </React.StrictMode>
+
+    <DevSupport ComponentPreviews={ComponentPreviews}
+        useInitialHook={useInitial}
+    >
+        <App />
+    </DevSupport>
+
 );
